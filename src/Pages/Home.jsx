8cf8b10353864aebd162d1da89c6920df002e5bf
santheartist <vazquezbare@gmail.com>
2023-06-13 21:48:21 -0300

    audio

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -1,10 +1,16 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
+import Console from "../Components/Console";
 import Over from "../Components/Over";
 import Video from "../Components/Video";
 
 const Home = () => {
+
+  //
   const [gif, setGif] = useState(false);
+
+  const audioRef = useRef(null);
+
 
   useEffect(() => {
     const handleKeyDown = () => {
@@ -20,13 +26,22 @@ const Home = () => {
     };
   }, []);
 
+  useEffect(() => {
+    if (gif && audioRef.current) {
+      audioRef.current.play();
+    } else if (!gif && audioRef.current) {
+      audioRef.current.pause();
+      audioRef.current.currentTime = 0; // Optional: reset audio to the start
+    }
+  }, [gif]);
+
   return (
     <div className="home-wrapper">
-      
+
       {gif ? <Over /> : null}
+      <audio ref={audioRef} src={'/sounds/keyboard-stress-36591.mp3'} />
 
       <Video />
-
     </div>
   );
 };
